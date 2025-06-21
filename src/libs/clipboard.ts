d77506dcfedb04d9106fa6dd146103170cd865d7
eonsang <2eonhworking@gmail.com>
2025-06-21 16:15:27 +0900

    sdk 작업중

--- src/libs/clipboard.ts
+++ src/libs/clipboard.ts
@@ -7,4 +7,12 @@ export class Clipboard {
   constructor(bridge: WebViewBridge) {
     this.bridge = bridge;
   }
+
+  getText(callback: (text: string) => void) {
+    this.bridge.sendMessage(MESSAGE_KEY.clipboard.getText, callback);
+  }
+
+  setText(text: string, callback: (success: boolean) => void) {
+    this.bridge.sendMessage(MESSAGE_KEY.clipboard.setText, callback, text);
+  }
 }
